4abcc2acaf6cf85bb151b1fab34b0a9754a00397
apcompscimiller <bths@gmail.com>
2025-12-02 11:04:18 -0500

    adveny

---
+++ src/Day2.java
@@ -0,0 +1,22 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Day2 {
+    public static void main(String[] args) {
+        int sum = 0;
+        String input = "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124";
+        String[] items = input.split(",");
+
+        for (int i = 0; i <  items.length; i++){
+                String[] nums = items[i].split("-");
+            }
+        }
+    }
+
+    public static void check(int num){
+        if num
+    }
+
+}
