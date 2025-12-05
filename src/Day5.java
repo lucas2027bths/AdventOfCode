523d9a3e3167ffcf74d7ca0a638707c3c982ca8f
das class <a@nycstudents.net>
2025-12-05 10:38:27 -0500

    day 5 part 2

--- src/Day5.java
+++ src/Day5.java
@@ -2,44 +2,35 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.Collections;
 public class Day5 {
 
     public static void main(String[] args) {
         ArrayList<String> fileData = getFileData("src/day5");
-        ArrayList<Long> numbers = new ArrayList<>();
+        ArrayList<Long> mins = new ArrayList<>();
+        ArrayList<Long> maxs = new ArrayList<>();
         int i = 0;
-        int amountOfFresh = 0;
+        long amountOfFresh = 0;
         String currentInstruction = "n/a";
             while (i < fileData.size()) {
                 currentInstruction = fileData.get(i);
                 if (!currentInstruction.isEmpty()){
+                    long min = Long.parseLong(currentInstruction.split("-")[0]);
+                    long max = Long.parseLong(currentInstruction.split("-")[1]);
+                    mins.add(min);
+                    maxs.add(max);
                     i++;
                 }else{
                     break;
                 }
             }
-        for (int x = i+1; x < fileData.size(); x++){
-            currentInstruction = fileData.get(x);
-            long num = Long.parseLong(currentInstruction);
-            numbers.add(num);
-        }
-        for (int y = 0; y < fileData.size(); y++){
-            currentInstruction = fileData.get(y);
-            if (!currentInstruction.isEmpty()){
-                long min = Long.parseLong(currentInstruction.split("-")[0]);
-                long max = Long.parseLong(currentInstruction.split("-")[1]);
-                for (int z = 0; z < numbers.size(); z++){
-                    if (numbers.get(z) >= min && numbers.get(z)<=max){
-                        numbers.remove(z);
-                        z-=1;
-                        amountOfFresh+=1;
-                    }
-                }
-            }else{
-                break;
-            }
-        }
-        System.out.println(numbers);
+            Collections.sort(mins);
+        Collections.sort(maxs);
+        System.out.println(mins);
+        System.out.println(maxs);
+
+
+
         System.out.println(amountOfFresh);
     }
     public static ArrayList<String> getFileData(String fileName) {
