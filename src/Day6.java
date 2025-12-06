51537e8d003c6c2e7ac8d2fd7a92afd209f9162d
lhome <bths@gmail.com>
2025-12-06 13:07:03 -0500

    day 6 part 2 attempt (doesn twork)

--- src/Day6.java
+++ src/Day6.java
@@ -1,4 +1,7 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Day6 {
     ArrayList<String> fileData = getFileData("src/day6");
@@ -11,8 +14,9 @@ public class Day6 {
             data.add(instructions);
         }
         int dataSize = data.size();
-        for (int x = 0; x < data.get(1).length; x++){
+        for (int x =    0; x < data.get(1).length; x++){
             long currentSum;
+            System.out.println("New column started");
             String operator = data.get(dataSize-1)[x];
             if (operator.equals("+")){
                 currentSum = 0;
@@ -20,24 +24,59 @@ public class Day6 {
             else{
                 currentSum = 1;
             }
+            int greatestLength = 0;
             for (int i = 0; i < dataSize - 1; i++){
-                int num = Integer.parseInt(data.get(i)[x]);
-                System.out.println(data.get(i)[x]);
+                String num = data.get(i)[x];
+                int length = num.length();
+                if (length > greatestLength){
+                    greatestLength = length;
+                }
+            }
+            System.out.println("Our Greatest Length: " + greatestLength);
+            for (int f = greatestLength-1; f > -1;f--){
+                String totalNum = "";
+                //get nums
+                for (int i = 0; i < dataSize - 1; i++){
+                    String num = data.get(i)[x];
+                    String desiredPart = "";
+                    if (num.length()-1 >= f && num.charAt(f)!='.'){
+                        desiredPart = "" + num.charAt(f);
+                    }
+                    System.out.println("Our Original Number is" + num);
+                    System.out.println("Our desired part is " + desiredPart);
+                    totalNum += desiredPart;
+                }
+                System.out.println("total num is" + totalNum);
+                long num = Integer.parseInt(totalNum);
                 if (operator.equals("+")){
                     currentSum += num;
                 }else{
                     currentSum *= num;
                 }
-                System.out.println("Current Sum" + currentSum + "Operator is" + operator);
+                System.out.println("Current sum is " + currentSum + "The Operator should be " +operator);
             }
+            System.out.println("THE FINAL CURRENT SUM IS " + currentSum);
+            System.out.println();
             sum += currentSum;
         }
-        System.out.println(sum);
+//        System.out.println(sum);
     }
 
 
     public static ArrayList<String> getFileData(String fileName) {
-        return Day5.getFileData(fileName);
+        ArrayList<String> fileData = new ArrayList<String>();
+        try {
+            File f = new File(fileName);
+            Scanner s = new Scanner(f);
+            while (s.hasNextLine()) {
+                String line = s.nextLine();
+                fileData.add(line);
+            }
+            return fileData;
+        }
+        catch (FileNotFoundException e) {
+            return fileData;
+        }
     }
 }
 
