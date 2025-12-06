6816285f8284d576abad3b8312a8ca07139ea46e
lhome <bths@gmail.com>
2025-12-05 20:46:35 -0500

    day 5

--- src/Day5.java
+++ src/Day5.java
@@ -1,46 +1,50 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.Scanner;
-import java.util.Collections;
+import java.util.*;
 public class Day5 {
 
     public static void main(String[] args) {
         ArrayList<String> fileData = getFileData("src/day5");
-        ArrayList<Long> mins = new ArrayList<>();
-        ArrayList<Long> maxs = new ArrayList<>();
+        class Range {
+            public long min = 0;
+            public long max = 0;
+
+            public Range(long min, long max) {
+                this.min = min;
+                this.max = max;
+            }
+        }
+        ArrayList<Range> ranges = new ArrayList<>();
         int i = 0;
         long amountOfFresh = 0;
         String currentInstruction = "n/a";
-            while (i < fileData.size()) {
-                currentInstruction = fileData.get(i);
-                if (!currentInstruction.isEmpty()){
-                    long min = Long.parseLong(currentInstruction.split("-")[0]);
-                    long max = Long.parseLong(currentInstruction.split("-")[1]);
-                    mins.add(min);
-                    maxs.add(max);
-                    i++;
-                }else{
-                    break;
-                }
-            }
-            Collections.sort(mins);
-        Collections.sort(maxs);
-        for (int x = 0; x < maxs.size(); x++){
-            long currentMax = maxs.get(x);
-            long currentMin = mins.get(x);
-            if (x>0 && currentMin < maxs.get(x-1)){
-                currentMin = maxs.get(x-1);
-                currentMin+=1;
-            }
-            amountOfFresh+= Math.abs(currentMin-currentMax) + 1;
-            if (x>0 && currentMin == maxs.get(x-1)){
-                amountOfFresh-=1;
+        while (i < fileData.size()) {
+            currentInstruction = fileData.get(i);
+            if (!currentInstruction.isEmpty()) {
+                long min = Long.parseLong(currentInstruction.split("-")[0]);
+                long max = Long.parseLong(currentInstruction.split("-")[1]);
+                ranges.add(new Range(min, max));
+                i++;
+            } else {
+                break;
             }
         }
+        ranges.sort(Comparator.comparingLong(r -> r.min)); // I found out you can sort objects by using a comparator to tell it exactly what to sort by / compare which is cool. hopefully I just remember this for later since this is super helpful
+        long currentMin = ranges.get(0).min;
+        long currentMax = ranges.get(0).max;
+        for (int x = 1; x < ranges.size(); x++) { //this took me 4 hours i hate math
+            Range r = ranges.get(x);
+            if (r.min <= currentMax + 1) {
+                currentMax = Math.max(currentMax, r.max);
+            } else {
+                amountOfFresh += (currentMax - currentMin + 1);
+                currentMin = r.min;
+                currentMax = r.max;
+            }
+        }
+        amountOfFresh += (currentMax - currentMin + 1);
+
         System.out.println(amountOfFresh);
-        System.out.println(mins);
-        System.out.println(maxs);
     }
     public static ArrayList<String> getFileData(String fileName) {
         ArrayList<String> fileData = new ArrayList<String>();
